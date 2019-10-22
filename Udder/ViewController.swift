b11716e722114c03e234622cab991778e7a38f0b
shun2k <gerogeroshocker@gmail.com>
2019-10-22 10:36:25 +0900

    Initial commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by 岸本俊祐 on 2019/10/22.
+//  Copyright © 2019 岸本俊祐. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
